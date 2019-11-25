7957565e305737310c49eb41089d33da268af152
Alvin Ferron <aferron@uoguelph.ca>
2019-11-25 05:08:42 -0500

    Update Passage layouts in the Layouts class

--- src/control/Layouts.java
+++ src/control/Layouts.java
@@ -122,18 +122,18 @@ public class Layouts {
         "| ## |            ",
         "| ## [-----+      ",
         "| ######## |      ",
-        "+~~~~~} ## [D1---+",
+        "+~~~~~} ## [DA---+",
         "      | ######## |",
         "      +~~~~~~~~~~+"
     };
 
     private static final String[] passage2 = new String[] {
         "                  ",
-        "  +---D1-------+  ",
+        "  +---DA-------+  ",
         "  | ########## |  ",
         "  +~~~~~~~} ## |  ",
         "          | ## |  ",
-        "  +---D1--] ## |  ",
+        "  +---DA--] ## |  ",
         "  | ########## |  ",
         "  +~~~~~~~~~~~~+  "
     };
@@ -141,7 +141,7 @@ public class Layouts {
     private static final String[] passage3 = new String[] {
         "                  ",
         "+-D1-+            ",
-        "| ## |  +---D1---+",
+        "| ## |  +---DA---+",
         "| ## |  | ###### |",
         "| ## |  | ## {~~~+",
         "| ## [--] ## |    ",
@@ -162,26 +162,26 @@ public class Layouts {
 
     private static final String[] passage5 = new String[] {
         "                  ",
-        "+-D1-+            ",
-        "| ## |  +---D1---+",
-        "| ## |  | ###### |",
-        "| ## |  | ## {~~~+",
-        "| ## [--] ## |    ",
-        "| ########## |    ",
-        "+~~~~~~~~~~~~+    "
+        "            +-D1-+",
+        "            | ## |",
+        "            | ## |",
+        "+-D1--------] ## |",
+        "| ############## |",
+        "+~~~~~~~~~~~~~~~~+",
+        "                  ",
     };
 
     private static final HashMap<Integer, String> descriptions = new HashMap<>() {{
-        put(1, String.format("Square, 6x6, Area: %d, Doors: %d", getArea(square), numDoors(square)));
-        put(2, String.format("Rectangle, 6x3, Area: %d, Doors: %d", getArea(rectangle), numDoors(rectangle)));
-        put(3, String.format("Circle, ?x?, Area: %d, Doors: %d", getArea(circle), numDoors(circle)));
-        put(4, String.format("Oval, ?x?, Area: %d, Doors: %d", getArea(oval), numDoors(oval)));
-        put(5, String.format("Unusual Shape, ?x?, Area: %d, Doors: %d", getArea(unusual1), numDoors(unusual1)));
-        put(6, String.format("Unusual Shape, ?x?, Area: %d, Doors: %d", getArea(unusual2), numDoors(unusual2)));
-        put(7, String.format("Unusual Shape, ?x?, Area: %d, Doors: %d", getArea(unusual3), numDoors(unusual3)));
-        put(8, String.format("Unusual Shape, ?x?, Area: %d, Doors: %d", getArea(unusual4), numDoors(unusual4)));
-        put(9, String.format("Unusual Shape, ?x?, Area: %d, Doors: %d", getArea(unusual5), numDoors(unusual5)));
-        put(10, String.format("Unusual Shape, ?x?, Area: %d, Doors: %d", getArea(unusual6), numDoors(unusual6)));
+        put(1, String.format("Square\nDimensions: 7x7\nArea: %d\nDoors: %d", getArea(square), numDoors(square)));
+        put(2, String.format("Rectangle\nDimensions: 7x4\nArea: %d\nDoors: %d", getArea(rectangle), numDoors(rectangle)));
+        put(3, String.format("Circle\nDimensions: ?x?\nArea: %d\nDoors: %d", getArea(circle), numDoors(circle)));
+        put(4, String.format("Oval\nDimensions: ?x?\nArea: %d\nDoors: %d", getArea(oval), numDoors(oval)));
+        put(5, String.format("Unusual Shape\nDimensions: ?x?\nArea: %d\nDoors: %d", getArea(unusual1), numDoors(unusual1)));
+        put(6, String.format("Unusual Shape\nDimensions: ?x?\nArea: %d\nDoors: %d", getArea(unusual2), numDoors(unusual2)));
+        put(7, String.format("Unusual Shape\nDimensions: ?x?\nArea: %d\nDoors: %d", getArea(unusual3), numDoors(unusual3)));
+        put(8, String.format("Unusual Shape\nDimensions: ?x?\nArea: %d\nDoors: %d", getArea(unusual4), numDoors(unusual4)));
+        put(9, String.format("Unusual Shape\nDimensions: ?x?\nArea: %d\nDoors: %d", getArea(unusual5), numDoors(unusual5)));
+        put(10, String.format("Unusual Shape\nDimensions: ?x?\nArea: %d\nDoors: %d", getArea(unusual6), numDoors(unusual6)));
     }};
 
     private static final HashMap<Integer, String[]> chambers = new HashMap<>() {{
@@ -203,6 +203,14 @@ public class Layouts {
         put(3, passage3);
         put(4, passage4);
         put(5, passage5);
+    }};
+
+    private static final HashMap<Integer, int[]> passageSections = new HashMap<>() {{
+        put(1, new int[] {1, 1, 11, 1, 1, 13, 1, 11, 1, 9, 19});
+        put(2, new int[] {1, 9, 1, 1, 13, 1, 1, 1, 13, 1, 1, 7, 19});
+        put(3, new int[] {1, 1, 1, 1, 11, 1, 1, 1, 11, 1, 1, 13, 9, 19});
+        put(4, new int[] {1, 1, 11, 1, 1, 1, 1, 1, 11, 1, 1, 5});
+        put(5, new int[] {11, 1, 1, 1, 1, 1, 11, 1, 1, 5});
     }};
 
     public static String[] chamber(int i) {
@@ -253,6 +261,10 @@ public class Layouts {
             }
         }
         return doors;
+    }
+
+    public static int[] sectionSequence(int i) {
+        return passageSections.get(i);
     }
 
 }
